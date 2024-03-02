38575394f69c05fef3a11d1ceee355fd58269299
Vivek Thangathurai <vivek.thangathurai@appdirect.com>
2024-03-02 23:55:29 +0530

    add icons and correct banner

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,10 +1,4 @@
   
-  // Function to close the info banner
-  function closeBanner() {
-    var banner = document.getElementById("infoBanner");
-    banner.style.display = "none";
-  }
-  
   document.addEventListener("DOMContentLoaded", function() {
     // This code will be executed when the entire document is loaded
     var banner = document.getElementById("infoBanner");
@@ -12,3 +6,19 @@
       banner.style.display = "block";
     }
   });
+
+  function showBanner() {
+    var banner = document.getElementById("infoBanner");
+    banner.style.display = "block";
+  }
+
+  function hideBanner() {
+    var banner = document.getElementById("infoBanner");
+    banner.style.display = "none";
+  }
+  
+  // Show the banner initially
+  showBanner();
+  
+  // Hide the banner after 1 minute
+  setTimeout(hideBanner, 300000); // 60000 milliseconds = 1 minute
